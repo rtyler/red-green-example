b0edeaa48897d09d8a04eaa099758bd8a459d54f
R. Tyler Croy <tyler@monkeypox.org>
2012-09-26 01:30:27 -0700

    Turns out blimpy-cucumber doesn't symlink modules properly

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,11 @@
 require 'rubygems'
 require 'blimpy/cucumber'
+require 'fileutils'
+
+Before do
+  # This is some hacky mess because I realized too late that blimpy-cucumber
+  # doesn't properly link up modules
+
+  # We should be in tmp/cucumber at this point
+  FileUtils.ln_s(File.expand_path(Dir.pwd + '/../../modules'), work_dir)
+end
